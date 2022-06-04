44c1222824af91ed76bb1132fc1e8cec2c6bf24d
Christopher Hileman <christopherhileman89@gmail.com>
2022-06-03 22:04:51 -0400

    Add computerPlayer function
    Added random choice for computer player.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+function computerPlay() {
+    number = Math.floor(Math.random() * 100) + 1;
+
+    if (number < 33) {
+        return 'Rock';
+    } else if (number >= 33 && number <= 66) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}

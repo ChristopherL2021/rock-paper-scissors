313b6776993e156826f58e2b3c3256dafcc51862
Christopher Hileman <christopherhileman89@gmail.com>
2022-06-04 21:26:07 -0400

    Add playRound function
    Added function to play one round of game

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ function computerPlay() {
         return 'scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
+        return "You did not enter rock, paper or scissors";
+    }
+
+    if (playerSelection === computerSelection) {
+        return playerSelection + " ties " + computerSelection;
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return playerSelection + " beats " + computerSelection + ", player wins";
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return playerSelection + " beats " + computerSelection + ", player wins";
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return playerSelection + " beats " + computerSelection + ", player wins";
+    } else {
+        return computerSelection + " beats " + playerSelection + ", computer wins";
+    }
+}

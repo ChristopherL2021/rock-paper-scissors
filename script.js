285b947a89744be73ca35f14541fce12183da866
Christopher Hileman <christopherhileman89@gmail.com>
2022-07-16 23:20:49 -0400

    update game logic

--- script.js
+++ script.js
@@ -12,56 +12,65 @@ function computerPlay() {
 }
 
 // Plays a single round of the game
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-
-    if (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
-        return "You did not enter rock, paper or scissors";
-    }
+function playRound(playerSelection) {
+    const computerSelection = computerPlay();
 
     if (playerSelection === computerSelection) {
-        return playerSelection + " ties " + computerSelection;
+        return capitalize(playerSelection) + " ties " + capitalize(computerSelection);
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         playerScore += 1;
-        return playerSelection + " beats " + computerSelection + ", player wins";
+        return capitalize(playerSelection) + " beats " + capitalize(computerSelection) + ", Player wins";
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         playerScore += 1;
-        return playerSelection + " beats " + computerSelection + ", player wins";
+        return capitalize(playerSelection) + " beats " + capitalize(computerSelection) + ", Player wins";
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         playerScore += 1;
-        return playerSelection + " beats " + computerSelection + ", player wins";
+        return capitalize(playerSelection) + " beats " + capitalize(computerSelection) + ", Player wins";
     } else {
         computerScore += 1;
-        return computerSelection + " beats " + playerSelection + ", computer wins";
+        return capitalize(computerSelection) + " beats " + capitalize(playerSelection) + ", Computer wins";
     }
 }
 
+// Capitalize first letter of selection
+function capitalize(string) {
+    return string[0].toUpperCase() + string.substring(1);
+}
+
+const winner = document.querySelector('#winner');
 // Finds the winner of the game
-function winner(playerScore, computerScore) {
-    if (playerScore === computerScore) {
-        return "Its a tie";
-    } else if (playerScore > computerScore) {
-        return "Player wins " + playerScore + " to " + computerScore;
+function findWinner(playerScore, computerScore) {
+    if (playerScore > computerScore) {
+        winner.textContent = `Player beats Computer ${playerScore} to ${computerScore}.`;
     } else {
-        return "Computer wins " + computerScore + " to " + playerScore;
+        winner.textContent = `Computer beats Player ${computerScore} to ${playerScore}.`;
     }
-}
-
-// Main game function
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Enter rock, paper or scissors")
-        const computerSelection = computerPlay();
-
-        console.log(playRound(playerSelection, computerSelection));
-    }
-    
-    console.log(winner(playerScore, computerScore));
 }
 
 // Sets scores for player and computer
 let playerScore = 0;
 let computerScore = 0;
+const results = document.querySelector('#results');
+const score = document.querySelector('#score');
 
-// Starts the game
-game();
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (winner) {
+            winner.textContent = '';
+        }
+        
+        const round = playRound(button.id);
+
+        results.textContent = round;
+        score.textContent = `Computer ${computerScore}, Player ${playerScore}.`;
+        
+        console.log(round);
+
+        if (playerScore === 5 || computerScore === 5) {
+            console.log(findWinner(playerScore, computerScore))
+            playerScore = 0;
+            computerScore = 0;
+        }
+    })
+});

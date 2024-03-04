df060718afd450c650f91b241dead63b1a11afb7
Christopher Hileman <christopherhileman89@gmail.com>
2024-03-04 08:35:43 -0500

    Implement check for round winner

--- script.js
+++ script.js
@@ -4,10 +4,22 @@ function playGame() {
   const computerChoices = ["rock", "paper", "scissors"];
   let playerSelection;
   let computerSelection;
+  let playerScore = 0;
+  let computerScore = 0;
 
   function getComputerChoice() {
     let randomChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     return randomChoice;
+  }
+
+  function isPlayerWinner(playerSelection, computerSelection) {
+    if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")) {
+          return true;
+        } else {
+          return false;
+    }   
   }
 
   function playRound(playerSelection, computerSelection) {
@@ -16,12 +28,12 @@ function playGame() {
       return "It's a tie!";
     }
 
-    if ((playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "scissors" && computerSelection === "paper") ||
-        (playerSelection === "paper" && computerSelection === "rock")) {
+    if (isPlayerWinner) {
+      playerScore += 1;
       return `${playerSelection} beats ${computerSelection}, Player wins!`
     } else {
-      return `${computerSelection} beats ${playerSelection}, Computer wins!`
+      computerScore += 1;
+      return `${computerSelection} beats ${playerSelection}, Computer wins!`      
     }
   }
 
@@ -44,4 +56,6 @@ function playGame() {
   playerSelection = prompt("Pick Rock, Paper, or Scissors.", "Rock");
   computerSelection = getComputerChoice();
   console.log(playRound(playerSelection, computerSelection));
+
+  console.log(computerScore, playerScore);
 }

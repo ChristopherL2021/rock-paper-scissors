de9424db28ee5a153e61ac1770315e4ea82b8879
Christopher Hileman <christopherhileman89@gmail.com>
2024-03-04 08:27:09 -0500

    Add play round function

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
-const computerChoices = ["rock", "paper", "scissors"];
+"use strict";
 
-function getComputerChoice() {
-  let randomChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  console.log(randomChoice);
+function playGame() {
+  const computerChoices = ["rock", "paper", "scissors"];
+  let playerSelection;
+  let computerSelection;
+
+  function getComputerChoice() {
+    let randomChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    return randomChoice;
+  }
+
+  function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+      return "It's a tie!";
+    }
+
+    if ((playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "scissors" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "rock")) {
+      return `${playerSelection} beats ${computerSelection}, Player wins!`
+    } else {
+      return `${computerSelection} beats ${playerSelection}, Computer wins!`
+    }
+  }
+
+  playerSelection = prompt("Pick Rock, Paper, or Scissors.", "Rock");
+  computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
+  playerSelection = prompt("Pick Rock, Paper, or Scissors.", "Rock");
+  computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
+  playerSelection = prompt("Pick Rock, Paper, or Scissors.", "Rock");
+  computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
+  playerSelection = prompt("Pick Rock, Paper, or Scissors.", "Rock");
+  computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
+  playerSelection = prompt("Pick Rock, Paper, or Scissors.", "Rock");
+  computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
 }
